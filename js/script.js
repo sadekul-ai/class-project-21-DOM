667064ac495b4e53e219ed1f1a443fd58afe001d
sadekul-ai <sadekul.psl@gmail.com>
2025-07-30 16:59:06 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+
+// let box = document.getElementById("myBox");
+//   box.style.color = "red";
+//   box.style.backgroundColor = "yellow";
+//   box.style.fontSize = "20px"; 
+
+// let text = document.createElement('article');
+// article.textContent = 'Bangladesh is beautiful country';
+// document.body.appendChild(text);
+
+let body = document.getElementById('body');
+body.style.margin = '0'
+body.style.padding = '0'
+body.style.background = 'grey'
+
+
+let maiBox = document.getElementById('mainbox')
+
+let text = document.createElement('h1');
+ text.innerText = 'Bangladesh is a beautiful country';
+document.body.appendChild(text);
+text.style.color = 'red';
+text.style.border = '3px, solid black,';
+text.style.width = '800px';
+text.style.alignItems = 'center';
+text.style.fontSize = '50px';
+
+
+let box_1 = document.createElement('box-1');
+box_1.innerText = 'Shundorbon is good place for visit';
+document.body.appendChild(box_1);
+box_1.style.border = '3px, solid black,';
+box_1.style.marginRight = '50px'
+box_1.style.color = 'blue'
+box_1.style.fontSize = '30px';
+box_1.style.width = '800px';
+
+let box_2 = document.createElement('box-2');
+box_2.innerText = 'Coxes Bazar is a tourist place'
+document.body.appendChild(box_2);
+box_2.style.border = '3px, solid black,';
+box_2.style.color = 'yellow'
+box_2.style.fontSize = '30px';
+box_2.style.width = '800px';
+
+
+
+
+
+
+
+
+
