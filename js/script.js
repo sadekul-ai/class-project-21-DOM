8555fab456b5da8a1782098f9494068b9c1e64c5
sadekul-ai <sadekul.psl@gmail.com>
2025-08-01 12:15:38 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
 
+// https://www.google.com/search?q=dom+in+js&sca_esv=7e391c2c0531b813&sxsrf=AE3TifN9yz5tE8HnizkywO0tiqCuutydNQ%3A1754028639631&source=hp&ei=X1qMaJq8JOOKwbkP4IqLwQE&iflsig=AOw8s4IAAAAAaIxob9CFuxK5y4tWQySn2kio7qaEs1I7&oq=dom&gs_lp=Egdnd3Mtd2l6IgNkb20qAggAMgoQIxiABBiKBRgnMgoQIxiABBiKBRgnMgcQABiABBgTMgcQABiABBgTMgcQABiABBgTMgcQABiABBgTMgcQABiABBgTMgcQABiABBgTMgcQABiABBgTMgcQABiABBgTSNMnUABYkghwAHgAkAEAmAHaAqABmwaqAQcwLjEuMS4xuAEDyAEA-AEBmAIDoALRBsICChAjGPAFGMkCGCfCAg4QABiABBiKBRixAxiDAcICDhAuGIAEGLEDGMcBGNEDwgILEC4YgAQYigUYsQPCAg4QLhiABBiKBRixAxiDAcICCxAAGIAEGIoFGLEDmAMAkgcHMC4xLjEuMaAH0x2yBwcwLjEuMS4xuAfRBsIHBzItMS4xLjHIByw&sclient=gws-wiz
+
+// document.createElement(element) using for Create an HTML element
+// document.removeChild(element) using for Remove an HTML element
+// document.appendChild(element) using for Add an HTML element
+// document.replaceChild(new, old) using for Replace an HTML element
+// document.write(text) using for Write into the HTML output stream
+// document.getElementById(id) using for selection the Id
+// getElementsByClassName() using for selection the class 
+// getElementsByTagName() using for selection the tag
+// getElementsByAttributeName() using for selection the -----
+
+ //Selecting Elements: Using methods like getElementById(), getElementsByClassName(), getElementsByTagName(), 
+querySelector(), querySelectorAll()
+
+// Modifying Content: Changing textContent, innerText, innerHTML, or attribute values
+
+// Changing Styles: Modifying CSS properties using the style property.
+
+ // Adding/Removing Elements: Creating new elements with createElement() and appending them or removing existing ones.
+
+// Handling Events: Attaching event listeners (e.g., addEventListener()) to react to user interactions like clicks or key presses.
+
+// document.createElement and .innerText using for text writing
+// document.body.appendChild(text) using for print 
+// document.getElementById() using for selection the Id
+
+
 // let box = document.getElementById("myBox");
 //   box.style.color = "red";
 //   box.style.backgroundColor = "yellow";
